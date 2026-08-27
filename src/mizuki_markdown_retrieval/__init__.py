671d7dd15ff33cb1a2c23cf2bd464dd30818f9f6
CreamyCappuccino <131350682+CreamyCappuccino@users.noreply.github.com>
2026-08-27 15:03:00 +0800

    Expose Toolkit boundary helpers

--- src/mizuki_markdown_retrieval/__init__.py
+++ src/mizuki_markdown_retrieval/__init__.py
@@ -2,6 +2,13 @@ from .chunking import CHUNK_PROFILES, ChunkProfile, chunk_markdown
 from .config import FolderOverride, FolderPolicy, ScopeConfig, ScopeMode
 from .discovery import discover_markdown
 from .models import DocumentRef, IndexedMarkdownFile, MarkdownChunk
+from .toolkit_bridge import (
+    ToolkitUnavailableError,
+    make_source_query,
+    resolve_toolkit,
+    to_toolkit_chunk,
+    to_toolkit_document,
+)
 
 __all__ = [
     "CHUNK_PROFILES",
@@ -13,6 +20,11 @@ __all__ = [
     "MarkdownChunk",
     "ScopeConfig",
     "ScopeMode",
+    "ToolkitUnavailableError",
     "chunk_markdown",
     "discover_markdown",
+    "make_source_query",
+    "resolve_toolkit",
+    "to_toolkit_chunk",
+    "to_toolkit_document",
 ]
