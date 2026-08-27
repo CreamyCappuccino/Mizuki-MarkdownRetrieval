a445ba314f29fd9a0779d7be1d652453b637f15c
CreamyCappuccino <131350682+CreamyCappuccino@users.noreply.github.com>
2026-08-27 15:07:56 +0800

    Expose incremental refresh API

--- src/mizuki_markdown_retrieval/__init__.py
+++ src/mizuki_markdown_retrieval/__init__.py
@@ -1,7 +1,17 @@
 from .chunking import CHUNK_PROFILES, ChunkProfile, chunk_markdown
 from .config import FolderOverride, FolderPolicy, ScopeConfig, ScopeMode
 from .discovery import discover_markdown
+from .indexing import (
+    ChunkSnapshot,
+    DocumentSnapshot,
+    DocumentUpdate,
+    IndexPlan,
+    build_snapshot,
+    plan_index_updates,
+)
 from .models import DocumentRef, IndexedMarkdownFile, MarkdownChunk
+from .refresh import RefreshPlan, commit_refresh_state, prepare_refresh
+from .state_store import StateFormatError, load_state, save_state
 from .toolkit_bridge import (
     ToolkitUnavailableError,
     make_source_query,
@@ -13,18 +23,30 @@ from .toolkit_bridge import (
 __all__ = [
     "CHUNK_PROFILES",
     "ChunkProfile",
+    "ChunkSnapshot",
     "DocumentRef",
+    "DocumentSnapshot",
+    "DocumentUpdate",
     "FolderOverride",
     "FolderPolicy",
+    "IndexPlan",
     "IndexedMarkdownFile",
     "MarkdownChunk",
+    "RefreshPlan",
     "ScopeConfig",
     "ScopeMode",
+    "StateFormatError",
     "ToolkitUnavailableError",
+    "build_snapshot",
     "chunk_markdown",
+    "commit_refresh_state",
     "discover_markdown",
+    "load_state",
     "make_source_query",
+    "plan_index_updates",
+    "prepare_refresh",
     "resolve_toolkit",
+    "save_state",
     "to_toolkit_chunk",
     "to_toolkit_document",
 ]
